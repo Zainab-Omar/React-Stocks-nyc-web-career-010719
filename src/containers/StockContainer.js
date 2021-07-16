a0bac6449453e1ea3a8319bece311f9ed1f1e1a4
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-07-16 15:57:11 -0500

    update StockContainer

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -8,7 +8,7 @@ class StockContainer extends Component {
       <div>
         <h2>Stocks</h2>
         {
-         this.props.stocks.map(stock => <Stock key={stock.id} stock = {stock} stockAction = {props.addPortfolioId} />)
+         this.props.stocks.map(stock => <Stock key={stock.id} stock = {stock} stockAction = {this.props.addPortfolioId} />)
         }
       </div>
     );
